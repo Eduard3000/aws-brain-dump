1bc1c447b741afa3b9eeb4536bc1857d28e1d34c
Eduard <eduard.laera@gmx.net>
2017-01-31 11:45:49 +0100

    Create aws_get_initial_windows_administrator_pw.sh

---
+++ aws_get_initial_windows_administrator_pw.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#using aws cli, jq, your users default region and account
+aws ec2 get-password-data --instance-id $1 | jq -c -r '.PasswordData' | xargs echo -n | base64 -d key ../your_private_key.pem | xargs echo
