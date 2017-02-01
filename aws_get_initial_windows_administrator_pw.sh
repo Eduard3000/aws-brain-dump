1ab90719b365de1032bdcfc8991c27bdb8f1dec6
fusers <eduard.laera@gmx.net>
2017-02-01 13:50:19 +0100

    added ignore garbage to base64

--- aws_get_initial_windows_administrator_pw.sh
+++ aws_get_initial_windows_administrator_pw.sh
@@ -2,4 +2,4 @@
 
 #using aws cli, jq, your users default region and account, pass it the instanceid
 
-aws ec2 get-password-data --instance-id $1 | jq -c -r '.PasswordData' | xargs echo -n | base64 -d | openssl rsautl -decrypt -inkey ./your_private_key.pem | xargs echo
+aws ec2 get-password-data --instance-id $1 | jq -c -r '.PasswordData' | xargs echo -n | base64 -d -i | openssl rsautl -decrypt -inkey ./your_private_key.pem | xargs echo
