4f8c9f541854cad6be3f99bea0b5f9eb03ada835
Eduard <eduard.laera@gmx.net>
2017-02-01 13:41:15 +0100

    Corrected

--- aws_get_initial_windows_administrator_pw.sh
+++ aws_get_initial_windows_administrator_pw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#using aws cli, jq, your users default region and account
+#using aws cli, jq, your users default region and account, pass it the instanceid
 
-aws ec2 get-password-data --instance-id $1 | jq -c -r '.PasswordData' | xargs echo -n | base64 -d key ../your_private_key.pem | xargs echo
+aws ec2 get-password-data --instance-id $1 | jq -c -r '.PasswordData' | xargs echo -n | base64 -d | openssl rsautl -decrypt -inkey ./your_private_key.pem | xargs echo
