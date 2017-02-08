d4f6ec32d323f605e8d9bb382f71e150e93df5ee
fusers <eduard.laera@gmx.net>
2017-02-08 13:37:19 +0100

    Added key as input param and param count check

--- aws_get_initial_windows_administrator_pw.sh
+++ aws_get_initial_windows_administrator_pw.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-#using aws cli, jq, your users default region and account, pass it the instanceid
+#using aws cli, jq, your users default region and account, pass it the private key and InstanceId 
 
-aws ec2 get-password-data --instance-id $1 | jq -c -r '.PasswordData' | xargs echo -n | base64 -d -i | openssl rsautl -decrypt -inkey ./your_private_key.pem | xargs echo
+if [ -z "$1" ] || [ -z "$2" ]
+then
+echo "usage: "
+echo "$0 keyfile InstanceID"
+exit 1
+fi
+
+aws ec2 get-password-data --instance-id $2 | jq -c -r '.PasswordData' | xargs echo -n | base64 -d -i | openssl rsautl -decrypt -inkey $1 | xargs echo
